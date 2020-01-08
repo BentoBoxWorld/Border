c5f9f59a9747bd0559f1f38a0e1f12a361740204
Gabriele C <sgdc3.mail@gmail.com>
2020-01-09 00:42:49 +0100

    Add commands and storage

--- src/main/java/world/bentobox/border/Border.java
+++ src/main/java/world/bentobox/border/Border.java
@@ -1,22 +1,94 @@
 package world.bentobox.border;
 
+import com.github.yannicklamprecht.worldborder.api.BorderAPI;
+import com.github.yannicklamprecht.worldborder.api.WorldBorderApi;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-
+import org.bukkit.scheduler.BukkitRunnable;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 import world.bentobox.bentobox.api.addons.Addon;
+import world.bentobox.bentobox.database.Database;
+import world.bentobox.bentobox.database.objects.Island;
+import world.bentobox.border.commands.IslandBorderCommand;
 import world.bentobox.border.listeners.PlayerListener;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Consumer;
+
 public final class Border extends Addon {
-    
-    @Override
-    public void onLoad() {
-        // Nothing to do
+
+    private WorldBorderApi worldBorderApi;
+    private Database<BorderData> handler;
+    private Map<UUID, BorderData> borderCache;
+
+    public void getLevelsData(@NonNull UUID targetPlayer, Consumer<BorderData> callback) {
+        BorderData borderData = borderCache.get(targetPlayer);
+        if (borderData != null) {
+            callback.accept(borderData);
+            return;
+        }
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                String uniqueId = targetPlayer.toString();
+                BorderData data = Optional.ofNullable(handler.loadObject(uniqueId))
+                        .orElseGet(() -> new BorderData(uniqueId));
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        borderCache.put(targetPlayer, data);
+                        callback.accept(data);
+                    }
+                }.runTask(getPlugin());
+            }
+        }.runTaskAsynchronously(getPlugin());
+    }
+
+    public void uncachePlayer(@Nullable UUID uniqueId) {
+        BorderData data = borderCache.remove(uniqueId);
+        if (data == null) {
+            return;
+        }
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                handler.saveObject(data);
+            }
+        }.runTaskAsynchronously(getPlugin());
+    }
+
+    public void updateBorder(Player player, Location location) {
+        removeBorder(player); // Remove current
+        getLevelsData(player.getUniqueId(), data -> {
+            if (!data.isEnabled()) {
+                return;
+            }
+            Island island = getPlugin().getIslands().getIslandAt(location).orElse(null);
+            if (island == null) {
+                return;
+            }
+            worldBorderApi.setBorder(player, island.getProtectionRange() * 2, island.getCenter());
+        });
+    }
+
+    public void removeBorder(Player player) {
+        worldBorderApi.resetWorldBorderToGlobal(player);
     }
 
     @Override
     public void onEnable() {
-        Plugin plugin = Bukkit.getPluginManager().getPlugin("WorldBorderAPI");
+        worldBorderApi = BorderAPI.getApi();
+        handler = new Database<>(this, BorderData.class);
+        borderCache = new HashMap<>();
 
+        Plugin plugin = Bukkit.getPluginManager().getPlugin("WorldBorderAPI");
         if (plugin == null || !plugin.isEnabled()) {
             getLogger().warning("WorldBorderAPI not found. Download from https://github.com/yannicklamprecht/WorldBorderAPI/releases");
             this.setState(State.DISABLED);
@@ -24,13 +96,25 @@ public final class Border extends Addon {
         }
 
         // Register listeners
-        this.registerListener(new PlayerListener());
-        
+        registerListener(new PlayerListener(this));
+
+        // Register commands
+        getPlugin().getAddonsManager().getGameModeAddons().forEach(gameModeAddon -> {
+            log("Border hooking into " + gameModeAddon.getDescription().getName());
+            gameModeAddon.getPlayerCommand().ifPresent(c -> new IslandBorderCommand(this, c, "border"));
+        });
     }
 
     @Override
     public void onDisable() {
-        // Nothing to do here
+        if (borderCache != null) {
+            borderCache.values().forEach(handler::saveObject);
+        }
+    }
+
+    @Nullable
+    public Database<BorderData> getHandler() {
+        return handler;
     }
 
 }
--- src/main/java/world/bentobox/border/listeners/PlayerListener.java
+++ src/main/java/world/bentobox/border/listeners/PlayerListener.java
@@ -1,69 +1,70 @@
 package world.bentobox.border.listeners;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
-
-import com.github.yannicklamprecht.worldborder.api.BorderAPI;
-
-import world.bentobox.bentobox.BentoBox;
 import world.bentobox.bentobox.api.events.island.IslandEvent.IslandDeleteEvent;
 import world.bentobox.bentobox.api.events.island.IslandEvent.IslandEnterEvent;
 import world.bentobox.bentobox.api.events.island.IslandEvent.IslandExitEvent;
+import world.bentobox.border.Border;
 
 /**
  * @author tastybento
- *
  */
 public class PlayerListener implements Listener {
 
-    private final BentoBox plugin = BentoBox.getInstance();
+    private final Border addon;
 
-    private void setBorder(Player player, Location location) {
-        BorderAPI.getApi().resetWorldBorderToGlobal(player);
-        plugin.getIslands().getIslandAt(location).ifPresent(island -> {
-            Bukkit.getScheduler().runTask(plugin, () -> BorderAPI
-                    .getApi()
-                    .setBorder(
-                            player,
-                            island.getProtectionRange() * 2,
-                            island.getCenter()));
-        });
+    public PlayerListener(Border addon) {
+        this.addon = addon;
     }
 
-    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
-    public void onPlayerEnterWorld(PlayerJoinEvent e) {
-        setBorder(e.getPlayer(), e.getPlayer().getLocation());
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        addon.updateBorder(e.getPlayer(), e.getPlayer().getLocation());
     }
 
-    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
-    public void onIslandExitEvent(IslandExitEvent e) {
-        BorderAPI.getApi().resetWorldBorderToGlobal(Bukkit.getPlayer(e.getPlayerUUID()));
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        addon.uncachePlayer(e.getPlayer().getUniqueId());
     }
 
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onIslandEnterEvent(IslandEnterEvent e) {
-        setBorder(Bukkit.getPlayer(e.getPlayerUUID()), e.getLocation());
-    }
-
-    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
-    public void onIslandDeleteEvent(IslandDeleteEvent e) {
-        if (e.getPlayerUUID() != null) {
-            OfflinePlayer p = Bukkit.getOfflinePlayer(e.getPlayerUUID());
-            if (p.isOnline()) {
-                BorderAPI.getApi().resetWorldBorderToGlobal(p.getPlayer());
-            }
+        Player player = addon.getServer().getPlayer(e.getPlayerUUID());
+        if (player == null) {
+            return;
         }
+        addon.updateBorder(player, e.getLocation());
     }
 
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onPlayerTeleport(PlayerTeleportEvent e) {
-        setBorder(e.getPlayer(), e.getTo());
+        addon.updateBorder(e.getPlayer(), e.getTo());
+    }
+
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+    public void onIslandExitEvent(IslandExitEvent e) {
+        Player player = addon.getServer().getPlayer(e.getPlayerUUID());
+        if (player == null) {
+            return;
+        }
+        addon.removeBorder(player);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onIslandDeleteEvent(IslandDeleteEvent e) {
+        if (e.getPlayerUUID() == null) {
+            return;
+        }
+        Player player = addon.getServer().getPlayer(e.getPlayerUUID());
+        if (player == null) {
+            return;
+        }
+        addon.removeBorder(player);
     }
 }
